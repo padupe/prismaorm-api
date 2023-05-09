1e149d86e6d5f062c8c952e1b732facea221e259
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2023-05-09 09:25:13 -0300

    refactor(src/posts/repositories/post.repository.ts): change method at "create".
    Signed-off-by: Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>

--- src/posts/repositories/posts.repository.ts
+++ src/posts/repositories/posts.repository.ts
@@ -1,16 +1,46 @@
 import { Injectable } from '@nestjs/common'
+import { Prisma } from '@prisma/client'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { CreatePostDto } from '../dto/create-post.dto'
 import { PostEntity } from '../entities/post.entity'
 import { UpdatePostDto } from '../dto/update-post.dto'
+import { NotFoundError } from 'src/common/errors/types/notFoundError'
 
 @Injectable()
 export class PostsRepository {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(createPostDto: CreatePostDto): Promise<PostEntity> {
+    const { authorEmail } = createPostDto
+
+    // Método para remover um item do objeto
+    delete createPostDto.authorEmail
+
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: authorEmail,
+      },
+    })
+
+    if (!user) {
+      throw new NotFoundError('Author not found!')
+    }
+
+    const data: Prisma.PostCreateInput = {
+      /*
+        Recebo todos os parâmetros do objeto, com exceção do "authorEmail"
+        Visto que esta propriedade foi removida acima.
+      */
+      ...createPostDto,
+      author: {
+        connect: {
+          email: authorEmail,
+        },
+      },
+    }
+
     return this.prisma.post.create({
-      data: createPostDto,
+      data,
     })
   }
 
