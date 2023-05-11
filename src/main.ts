f544fcb50a43d41ede9758abfb50ae56f3a576d3
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2023-05-11 18:04:55 -0300

    feat: install Swagger for documentation.
    Signed-off-by: Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,24 @@ import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthoriz
 import { NotFoundInterceptor } from './common/errors/interceptors/notFound.interceptor'
 import { ConflictInterceptor } from './common/errors/interceptors/conflict.interceptor'
 import { DatabaseInterceptor } from './common/errors/interceptors/database.interceptor'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  /**
+   * DocumentBuilder é o método que nos permite configurar a documentação
+   * Swagger, desde que as informações estejam de acordo com o padrão openapi.
+   */
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Simple Blog')
+    .setDescription('The Simple Blog API')
+    .setVersion('1.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig)
+  SwaggerModule.setup('docs', app, document)
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
