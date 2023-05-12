614648968a5030a49ffc065f1abebac499237011
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2023-05-12 09:57:06 -0300

    feat(src/users/dto/create-user.dto.ts): use "ApiProperty" to Swagger Doc.
    Signed-off-by: Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator'
 
 export class CreateUserDto {
+  @ApiProperty({ description: 'User e-mail' })
   @IsEmail()
   @IsNotEmpty()
   email: string
 
+  @ApiProperty({ description: 'User Full Name' })
   @IsString()
   @IsNotEmpty()
   name: string
 
+  @ApiProperty({ description: 'User is Admin', default: false })
   @IsBoolean()
   admin: boolean
 }
