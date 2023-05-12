5196628eeb155eb8a53d2700f1ee1b62f728d92f
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2023-05-12 10:07:18 -0300

    feat(src/posts/posts.controller.ts): use "ApiForbiddenResponse" decorator for Swagger Doc.
    Signed-off-by: Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -10,31 +10,37 @@ import {
 import { PostsService } from './posts.service'
 import { CreatePostDto } from './dto/create-post.dto'
 import { UpdatePostDto } from './dto/update-post.dto'
+import { ApiForbiddenResponse } from '@nestjs/swagger'
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  @ApiForbiddenResponse({ description: 'Denied Access' })
   @Post()
   create(@Body() createPostDto: CreatePostDto) {
     return this.postsService.create(createPostDto)
   }
 
+  @ApiForbiddenResponse({ description: 'Denied Access' })
   @Get()
   findAll() {
     return this.postsService.findAll()
   }
 
+  @ApiForbiddenResponse({ description: 'Denied Access' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.postsService.findOne(+id)
   }
 
+  @ApiForbiddenResponse({ description: 'Denied Access' })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
     return this.postsService.update(+id, updatePostDto)
   }
 
+  @ApiForbiddenResponse({ description: 'Denied Access' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.postsService.remove(+id)
