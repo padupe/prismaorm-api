b8c59f0a6b51efaca4707ee7297b9eb61c4215d1
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2023-05-12 09:57:41 -0300

    refactor(src/posts/dto/update-post.dto.ts): change import for use at Doc API.
    Signed-off-by: Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>

--- src/posts/dto/update-post.dto.ts
+++ src/posts/dto/update-post.dto.ts
@@ -1,4 +1,5 @@
-import { PartialType } from '@nestjs/mapped-types'
+// Usando PartialType do pacote Swagger, a documentação é preenchida automaticamente
+import { PartialType } from '@nestjs/swagger'
 import { CreatePostDto } from './create-post.dto'
 
 export class UpdatePostDto extends PartialType(CreatePostDto) {}
