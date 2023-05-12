0d8a9993ed33dbe140187893163e44ed2f0d9d00
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2023-05-12 10:11:14 -0300

    feat(src/users/users.controller.ts): use "ApiResponse" and "ApiForbiddenResponse" decorators for Swagger Doc.
    Signed-off-by: Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,31 +10,38 @@ import {
 import { UsersService } from './users.service'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
+import { ApiForbiddenResponse, ApiResponse } from '@nestjs/swagger'
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiResponse({ status: 409, description: 'E-mail Conflict' })
+  @ApiForbiddenResponse({ description: 'Denied Access' })
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto)
   }
 
+  @ApiForbiddenResponse({ description: 'Denied Access' })
   @Get()
   findAll() {
     return this.usersService.findAll()
   }
 
+  @ApiForbiddenResponse({ description: 'Denied Access' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id)
   }
 
+  @ApiForbiddenResponse({ description: 'Denied Access' })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto)
   }
 
+  @ApiForbiddenResponse({ description: 'Denied Access' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id)
